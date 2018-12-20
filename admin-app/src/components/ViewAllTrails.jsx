430ba98736d83e80f9606fc003767bbf674bcdc1
nicola-20 <35260168+nicola-20@users.noreply.github.com>
2018-12-20 16:14:47 +0000

    moving to robs laptop

--- admin-app/src/components/ViewAllTrails.jsx
+++ admin-app/src/components/ViewAllTrails.jsx
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
-import * as api from '../api.js'
-import Trail from './Trail'
-import './css/ViewAllTrails.css'
+import React, { Component } from "react";
+import * as api from "../api.js";
+import Trail from "./Trail";
+import "./css/ViewAllTrails.css";
 
 class ViewAllTrails extends Component {
   state = {
     trails: []
-  }
+  };
   render() {
-    const { trails } = this.state
+    const { trails } = this.state;
     return (
       <main className="view-trails">
         <h2 className="header">List of Trails</h2>
         <ul>
-          {trails.map((trail) => {
-
-            return <Trail key={trail.id} removeDeletedTrail={this.removeDeletedTrail} trail={trail}/>
-
+          {trails.map(trail => {
+            return (
+              <Trail
+                key={trail.id}
+                removeDeletedTrail={this.removeDeletedTrail}
+                trail={trail}
+              />
+            );
           })}
         </ul>
       </main>
     );
   }
   componentDidMount() {
-    api.getAllTrails()
-      .then((trails) => {
-        this.setState({
-          trails
-        })
-      })
+    api.getAllTrails().then(trails => {
+      this.setState({
+        trails
+      });
+    });
   }
 
-  removeDeletedTrail = (trailId) => {
-
-    const filteredTrails = this.state.trails.filter(trail => trail.id !== trailId)
+  removeDeletedTrail = trailId => {
+    const filteredTrails = this.state.trails.filter(
+      trail => trail.id !== trailId
+    );
     this.setState({
-
       trails: filteredTrails
-    })
-  }
+    });
+  };
 }
-
 
 export default ViewAllTrails;
